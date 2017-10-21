598cb70280e595e7f85cb7ee2ac73b2ba2409cb8
krustybr <carlos.poles@griffithuni.edu.au>
2017-10-21 17:43:43 +1000

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by Carlos Poles on 21/10/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
